988c7ac7f44f18b24fe0713c5d21d3d9a4ffe370
Lon Wibberding <wibberding@gmail.com>
2022-06-06 09:43:23 -0700

    building models

--- HNT_Back/model/StoryRating.js
+++ HNT_Back/model/StoryRating.js
@@ -1,4 +1,4 @@
-//* Rating by one of a story. These should be averaged for each story to be displayed in the story show view.
+//* Rating by one listener of a story. These should be averaged for each story to be displayed in the story show view.
 
 const mongoose = require('mongoose'); 
 const Schema = mongoose.Schema;
@@ -7,8 +7,8 @@ const StoryRating = new Schema({
   story_id: String,
   account_id: String, //* The user account that created the rating.
   time_stamp: Date, //* for adding up the ratings for the last week (7 days). May be built-in though.
-  //* Ratings should be whole numbers between 1 and 5 to correspond with the star rating system.
-  main_rating: Number, 
+  //* Ratings should be whole numbers between 0 and 5 to correspond with the star rating system.
+  enjoyment_rating: Number, 
   violence_rating: Number,
   sexual_content_rating: Number,
   lanaguage_rating: Number,
