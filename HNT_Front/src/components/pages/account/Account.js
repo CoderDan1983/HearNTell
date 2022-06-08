d6d3b9ad7f469d71ab3c4c2fa6126a96be0230ca
Lon Wibberding <wibberding@gmail.com>
2022-06-08 10:39:59 -0700

    6/8/22 worked on Account and Admin pages

--- HNT_Front/src/components/pages/account/Account.js
+++ HNT_Front/src/components/pages/account/Account.js
@@ -1,7 +1,61 @@
+import { useState } from "react";
+
+
 export default function Account(){
+    const [advertise, setAdvertise] = useState(false);
+    const [advertiseFocus, setAdvertiseFocus] = useState(false);
+
+    const [ payChoice, setPayChoice ] = useState("manual");
+    console.log(payChoice);
     return(
         <>
-            <h1 className="marketing">Account Page</h1>
+            <div className="main">
+                <h1 className="marketing">My Account</h1>
+                <h2>Support Hear N Tell and get rid of ads for: $8.95 for three months.</h2>
+                <div className="radioGroup">
+                    <input 
+                        type="radio" 
+                        name="payChoice" 
+                        value="manual" 
+                        id="manualPayChoice" 
+                        checked={ payChoice === "manual"}
+                        onChange={ (e)=> setPayChoice(e.target.value) }
+                    />
+                    <label htmlFor="manualPayChoice">Pay for three months, but don’t put me on automatic payments.</label>
+                </div>
+
+                <div className="radioGroup">
+                    <input 
+                        type="radio" 
+                        name="payChoice" 
+                        value="auto" 
+                        id="autoPayChoice"
+                        checked={ payChoice === "auto"}
+                        onChange={ (e)=> setPayChoice(e.target.value) }
+                    />
+                    <label htmlFor="autoPayChoice">Pay for three months and put me on automatic payments.</label>
+                </div>
+
+                <div className="radioGroup">
+                    <input 
+                        id="advertise"
+                        type="checkbox" 
+                        value={ advertise }
+                        // aria-invalid={validMatch ? "false" : "true"}
+                        aria-describedby="advertise"
+                        onFocus= { ()=> setAdvertiseFocus(true) }
+                        onBlur={ () => setAdvertiseFocus(false) }
+                        onClick={ (e)=> setAdvertise(!advertise) }
+                    />
+                    <label htmlFor="advertise">
+                        I would like to advertise on Hear N Tell
+                    </label><br />
+                </div>
+
+                <h3>Stripe info goes here</h3>
+            </div>
+
+
         </>
     )
 }
