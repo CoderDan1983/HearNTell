0b8a9ae88d7b7bb18e8b582ce2ea308fad1ab3d3
Lon Wibberding <wibberding@gmail.com>
2022-06-07 18:37:07 -0700

    minor changes

--- HNT_Front/src/components/pages/HomePublic.js
+++ HNT_Front/src/components/pages/HomePublic.js
@@ -6,6 +6,17 @@ const HomePublic = () => {
         <h1>Hear n Tell</h1>
         <div>A place for people to share stories</div>
         <h2>Top Stories this Week:</h2>
+
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8">
+                    This is an 8 wide column
+                </div>
+                <div className="col-md-4">
+                    This is a 4 wide column.
+                </div>
+            </div>
+        </div>
         
     </div>)
 }
