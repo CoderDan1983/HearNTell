0d2ebd65965ef94638a0860efec2a8facc28c47b
CoderDan1983 <seekingsomething121@gmail.com>
2022-06-08 10:41:55 -0700

    new as of 9:50 am, 6/8?

--- HNT_Front/src/components/parts/LinkListItem.js
+++ HNT_Front/src/components/parts/LinkListItem.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import '../../index.css';
 
 export default function LinkListItem({ to, name }){
+    // return(<Link to={ `${path}/${_id}/${path1}` }>
     return(<Link to={ to }>
         <div className="link">
             { name }
