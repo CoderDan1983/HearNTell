9303e089a3ec1cde6d6569f3ff23716e8ce2a2e2
Lon Wibberding <wibberding@gmail.com>
2022-06-10 18:08:56 -0700

    Builds storyRatingController

--- HNT_Back/routes/api/story_rating.js
+++ HNT_Back/routes/api/story_rating.js
@@ -8,11 +8,11 @@ const storyRatingController = require('../../controllers/storyRatingController.j
 // Create a new rating                         POST /api/rating 
 router.post('/', storyRatingController.create);
 
-// Get a single rating by story and user       GET /api/rating/user/{user_id}/story/{story_id}
-router.get('/user/:user_id/story/:story_id', storyRatingController.byUserAndStory);
+// Get a single rating by story and user       GET /api/rating/user/{account_id}/story/{story_id}
+router.get('/user/:account_id/story/:story_id', storyRatingController.byAccountAndStory);
 
 // Get all the ratings for a story             GET /api/rating/story/{story_id}
-router.get('/story//:story_id', storyRatingController.index);
+router.get('/story/:story_id', storyRatingController.index);
 
 // Get a story rating by id                    GET /api/rating/{story_rating_id}
 router.get('/:story_rating_id', storyRatingController.show);
