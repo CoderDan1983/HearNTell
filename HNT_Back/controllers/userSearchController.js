05ae766ce0113a5a5149054213094b46e50ee214
Lon Wibberding <wibberding@gmail.com>
2022-06-10 18:53:32 -0700

    Builds userSearchController

--- HNT_Back/controllers/userSearchController.js
+++ HNT_Back/controllers/userSearchController.js
@@ -3,20 +3,26 @@ const UserSearch = require('../model/UserSearch');
 
 //* Create (record) a new search
 const create = async (req, res) => {
-
-  res.json('');
+  const user_search_data = {
+    user_id: req.body.user_id,
+    search_string: req.body.search_string
+  };
+  let user_search = await UserSearch.create(user_search_data)
+  res.json(user_search);
 };
 
 //* Remove search record
 const remove = async (req, res) => {
-
-  res.json('');
+  const user_search_id = req.params.user_search_id;
+  let user_search = await UserSearch.findOneAndDelete({_id: user_search_id});
+  res.json(user_search);
 };
 
 //* Get searches by user
 const searchesByUser = async (req, res) => {
-
-  res.json('');
+  const user_id = req.params.user_id;
+  let searches = await UserSearch.find({user_id: user_id});
+  res.json(searches);
 };
 
 
